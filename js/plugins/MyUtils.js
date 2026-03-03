7f86ca2198450afddf822fe9a9b5d99fe568d6ee
AvianJay <jay20090728@gmail.com>
2026-03-03 15:11:32 +0800

    update story + fix plugin

--- js/plugins/MyUtils.js
+++ js/plugins/MyUtils.js
@@ -3,9 +3,9 @@
  * @plugindesc Some utility commands.
  * @author AvianJay
  * 
- * @command waitForClick
- * @text Wait for Click
- * @desc Set switch ON when the player clicks the screen
+ * @command waitForInput
+ * @text Wait for Input
+ * @desc Set switch ON when the player clicks the screen or presses any key
  * 
  * @arg switchId
  * @type switch
@@ -16,14 +16,31 @@
 
 (() => {
     const PLUGIN_NAME = "MyUtils";
-    PluginManager.registerCommand(PLUGIN_NAME, "waitForClick", (args) => {
-        return new Promise((resolve) => {
-            const onClick = () => {
-                document.removeEventListener("click", onClick);
-                $gameSwitches.setValue(Number(args.switchId), true);
-                resolve();
-            };
-            document.addEventListener("click", onClick);
-        });
+    let _waitingSwitch = null;
+
+    PluginManager.registerCommand(PLUGIN_NAME, "waitForInput", (args) => {
+        _waitingSwitch = Number(args.switchId);
+    });
+
+    // 在 Scene_Map.update 中偵測輸入
+    const _Scene_Map_update = Scene_Map.prototype.update;
+    Scene_Map.prototype.update = function() {
+        _Scene_Map_update.call(this);
+        if (_waitingSwitch != null) {
+            if (TouchInput.isTriggered() || Input._latestButton || _anyKeyPressed) {
+                const sw = _waitingSwitch;
+                _waitingSwitch = null;
+                _anyKeyPressed = false;
+                $gameSwitches.setValue(sw, true);
+            }
+        }
+    };
+
+    // 捕捉任意鍵盤按鍵
+    let _anyKeyPressed = false;
+    document.addEventListener("keydown", () => {
+        if (_waitingSwitch != null) {
+            _anyKeyPressed = true;
+        }
     });
 })();
